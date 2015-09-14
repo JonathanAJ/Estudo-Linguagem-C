5949ff699829ff20812355e72df8943d0fbfbe75
Jonathan Alves <alves_aj@windowslive.com>
2015-09-14 13:45:57 -0300

    Adicionando tipos de estruturas

---
+++ 8. Tipos Estruturados/tipo_estrutura.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct coordenada{
+	int x;
+	int y;
+}
+
+int main(void){
+	coordenada.x = 10;
+	coordenada.y = 20;
+	printf("Coordenada x é %d e y é %d.\n",coordenada.x, coordenada.y);
+	return 0;
+}
