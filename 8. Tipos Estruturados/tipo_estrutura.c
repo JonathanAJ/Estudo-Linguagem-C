6ec4c9c35dc98cb8420ae482b3a6757f7e1b5ddc
Jonathan Alves <alves_aj@windowslive.com>
2015-09-14 17:32:50 -0300

    Adicionando tipos estruturados

--- 8. Tipos Estruturados/tipo_estrutura.c
+++ 8. Tipos Estruturados/tipo_estrutura.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 
+/*
+ * Inicializando uma estrutura "coordenada" que irá
+ * agrupar duas variaveis x e y.
+ */
+
 struct coordenada{
 	int x;
 	int y;
-}
+};
 
 int main(void){
-	coordenada.x = 10;
-	coordenada.y = 20;
-	printf("Coordenada x é %d e y é %d.\n",coordenada.x, coordenada.y);
+	/*
+ 	 * Inicializando uma variavel cood1 que é
+ 	 * uma estrutura coordenada. Setando x e y com
+ 	 * o uso do ponto(.)
+	 */
+	struct coordenada cood1;
+	cood1.x = 10;
+	cood1.y = 20;
+	printf("Coordenada x é %d e y é %d.\n", cood1.x, cood1.y);
+	
+	/*
+	 * Abaixo utilizo o typedef que é utilizado para definir
+	 * novos tipos de dados. O código abaixo significa que
+	 * estou definindo um tipo de estrutura coordenada como
+	 * Coordenada. Após isso o novo tipo pode ser inicializado
+	 * com um contexto mais simples.
+	 */
+	typedef struct coordenada Coordenada;
+	/*
+	 * Criando uma variável do tipo Coordenada chamada cd1.
+	 */
+	Coordenada cd1;
+	cd1.x = 1;
+	cd1.y = 2;
+	printf("Coordenada x é %d e y é %d.\n", cd1.x, cd1.y);
+
 	return 0;
 }
