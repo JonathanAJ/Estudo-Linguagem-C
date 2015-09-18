0527ebde07b7d46d017762bcc009ec4c88003d5d
Jonathan Alves <alves_aj@windowslive.com>
2015-09-17 22:12:06 -0300

    adicionado comentários ao código

--- 12. Arvores/arvore_binaria.c
+++ 12. Arvores/arvore_binaria.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+ * Criação de estrutura árvore e definição de um novo tipo.
+ */
 struct arvore{
 	int arv_inf;
 	struct arvore *arv_esq;
 	struct arvore *arv_dir;
 };
 typedef struct arvore Arvore;
-
+/*
+ * Protótipos
+ */
 Arvore* criaArvoreVazia(void);
 Arvore* criaArvore(int arv_inf, Arvore *arv_esq, Arvore *arv_dir);
 void mostraArvore(Arvore *arv);
@@ -15,9 +19,9 @@ void mostraArvore(Arvore *arv);
  *	Árvore binária cheia representada
  *	pelo seguinte modelo:
  *
- *			 1
+ *           1
  *		   /   \
- *		  2		3
+ *		  2     3
  *		 / \   / \
  *		4   5 6   7
  */
@@ -47,11 +51,17 @@ int main(void){
 	mostraArvore(arv_my);
 	return 0;
 }
-
+/*
+ * Cria e retorna uma árvore vazia
+ */
 Arvore* criaArvoreVazia(void){
 	return NULL;
 }
-
+/*
+ * Aloca memória para uma nova arvore, depois seta mais
+ * duas outras árvores esquerda e direita, retorna o valor
+ * atualizado para não se perder quando a função terminar.
+ */
 Arvore* criaArvore(int arv_inf, Arvore *arv_esq, Arvore *arv_dir){
 	Arvore *arv_new = (Arvore*) malloc(sizeof(Arvore));
 	arv_new->arv_inf = arv_inf;
@@ -59,11 +69,13 @@ Arvore* criaArvore(int arv_inf, Arvore *arv_esq, Arvore *arv_dir){
 	arv_new->arv_dir = arv_dir;
 	return arv_new;
 }
-
+/*
+ * Função para mostrar a árvore em "pré-ordem".
+ */
 void mostraArvore(Arvore *arv){
 	if(arv!=NULL){
-		mostraArvore(arv->arv_esq);
 		printf("Nó: %d\n", arv->arv_inf);
+		mostraArvore(arv->arv_esq);
 		mostraArvore(arv->arv_dir);
 	}
 }
